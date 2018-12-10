f12d4de98e17c65a5e676a3060e63e10c01626b1
ncflagg <ncflagg@gmail.com>
2018-12-09 20:14:39 -0800

    GITHUB BEING STUPID

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -76,16 +76,7 @@ class CarInterface(object):
     ret.steerKiBP, ret.steerKpBP = [[0.], [0.]]
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
 
-    if candidate == CAR.PRIUS:
-      ret.safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
-      ret.wheelbase = 2.70
-      ret.steerRatio = 13.40   # unknown end-to-end spec
-      tire_stiffness_factor = 0.61   # hand-tune
-      ret.mass = 3375 * CV.LB_TO_KG + std_cargo
-      ret.steerKpV, ret.steerKiV = [[0.4], [0.01]]
-      ret.steerKf = 0.000078   # full torque for 10 deg at 80mph means 0.00007818594
-      # TODO: Prius seem to have very laggy actuators. Understand if it is lag or hysteresis
-      ret.steerActuatorDelay = 0.16
+GITHUB BEING STUPID
 
     elif candidate in [CAR.RAV4, CAR.RAV4H]:
       ret.safetyParam = 73  # see conversion factor for STEER_TORQUE_EPS in dbc file
