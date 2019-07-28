f5ddc4496214aa6e9bd06fbf0616d00edce221ab
ncflagg <ncflagg@gmail.com>
2019-07-27 23:28:06 -0700

    Smaller learning windows

--- selfdrive/locationd/params_learner.h
+++ selfdrive/locationd/params_learner.h
@@ -3,14 +3,14 @@
 #define DEGREES_TO_RADIANS 0.017453292519943295
 #define RADIANS_TO_DEGREES (1.0 / DEGREES_TO_RADIANS)
 
-#define MAX_ANGLE_OFFSET (10.0 * DEGREES_TO_RADIANS)
-#define MAX_ANGLE_OFFSET_TH  (9.0 * DEGREES_TO_RADIANS)
-#define MIN_STIFFNESS  0.5
-#define MAX_STIFFNESS  2.0
-#define MIN_SR  0.5
-#define MAX_SR  2.0
-#define MIN_SR_TH  0.55
-#define MAX_SR_TH  1.9
+#define MAX_ANGLE_OFFSET (2.0 * DEGREES_TO_RADIANS)      // 10.0
+#define MAX_ANGLE_OFFSET_TH  (9.0 * DEGREES_TO_RADIANS)  // 9.0
+#define MIN_STIFFNESS 0.7                                // 0.5
+#define MAX_STIFFNESS 1.2                                // 2.0
+#define MIN_SR 0.15                                      // 0.5
+#define MAX_SR 1.5                                       // 2.0
+#define MIN_SR_TH 0.2                                    // 0.55
+#define MAX_SR_TH 1.4                                    // 1.9
 
 class ParamsLearner {
   double cF0, cR0;
