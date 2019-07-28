2e63de367ab8a61a3eb580b82bd31bee6f50a59f
ncflagg <ncflagg@gmail.com>
2019-07-27 23:25:35 -0700

    Tuning and tire stiffness hacks

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -73,15 +73,15 @@ class CarInterface(object):
       stop_and_go = True
       ret.safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
       ret.wheelbase = 2.70
-      ret.steerRatio = 15.00   # unknown end-to-end spec
-      tire_stiffness_factor = 0.6371   # hand-tune
-      ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
+      ret.steerRatio = 15.0   # unknown end-to-end spec
+      tire_stiffness_factor = 0.75   # hand-tune
+      ret.mass = 3375. * CV.LB_TO_KG + STD_CARGO_KG
 
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGain = 4.0
-      ret.lateralTuning.indi.outerLoopGain = 3.0
-      ret.lateralTuning.indi.timeConstant = 1.0
-      ret.lateralTuning.indi.actuatorEffectiveness = 1.0
+      ret.lateralTuning.indi.innerLoopGain = 4.0          # 4.25
+      ret.lateralTuning.indi.outerLoopGain = 3.0          # 2.5
+      ret.lateralTuning.indi.timeConstant = 3.0           # 3.0
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.35  # 1.2
 
       ret.steerActuatorDelay = 0.5
 
