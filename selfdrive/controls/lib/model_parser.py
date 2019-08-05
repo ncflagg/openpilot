9883146a2bd1389df56683b7f65cfca1715b8120
Comma Device <device@comma.ai>
2019-08-04 23:46:51 -0700

    Zorro's lane width stuff

--- selfdrive/controls/lib/model_parser.py
+++ selfdrive/controls/lib/model_parser.py
@@ -1,9 +1,10 @@
-from common.numpy_fast import interp
 import numpy as np
 from selfdrive.controls.lib.latcontrol_helpers import model_polyfit, calc_desired_path, compute_path_pinv
 
 CAMERA_OFFSET = 0.06  # m from center car to camera
 
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
 
 class ModelParser(object):
   def __init__(self):
@@ -13,13 +14,12 @@ class ModelParser(object):
     self.last_model = 0.
     self.lead_dist, self.lead_prob, self.lead_var = 0, 0, 1
     self._path_pinv = compute_path_pinv()
-
-    self.lane_width_estimate = 3.0
-    self.lane_width_certainty = 1.0
-    self.lane_width = 3.0
     self.l_prob = 0.
     self.r_prob = 0.
     self.x_points = np.arange(50)
+    self.lane_width = 3
+    self.frame = 0
+    self.readings = []
 
   def update(self, v_ego, md):
     if len(md.leftLane.poly):
@@ -40,14 +40,17 @@ class ModelParser(object):
     r_prob = md.rightLane.prob  # right line prob
 
     # Find current lanewidth
-    lr_prob = l_prob * r_prob
-    decay_rate = interp(lr_prob, [0., 0.5], [0.1, 0.3])
-    decay_rate *= v_ego / 31.0
-    self.lane_width_certainty += 0.05 * decay_rate * (lr_prob - self.lane_width_certainty)
-    current_lane_width = abs(l_poly[3] - r_poly[3])
-    self.lane_width_estimate += 0.005 * decay_rate * (current_lane_width - self.lane_width_estimate)
-    speed_lane_width = interp(v_ego, [0., 31.], [3.0, 3.6])
-    self.lane_width = self.lane_width_certainty * self.lane_width_estimate + (1.0 - self.lane_width_certainty) * speed_lane_width
+    if l_prob > 0.49 and r_prob > 0.49:
+        self.frame += 1
+        if self.frame % 20 == 0:
+            self.frame = 0
+            current_lane_width = sorted((2.8, abs(l_poly[3] - r_poly[3]), 3.6))[1]
+            max_samples = 30
+            self.readings.append(current_lane_width)
+            avg = mean(self.readings)
+            self.lanewidth = avg
+            if len(self.readings) == max_samples:
+                self.readings.pop(0)
 
     self.lead_dist = md.lead.dist
     self.lead_prob = md.lead.prob
@@ -55,7 +58,7 @@ class ModelParser(object):
 
     # compute target path
     self.d_poly, self.c_poly, self.c_prob = calc_desired_path(
-      l_poly, r_poly, np.array(self.d_poly), l_prob, r_prob, p_prob, v_ego, self.lane_width)
+      l_poly, r_poly, p_poly, l_prob, r_prob, p_prob, v_ego, self.lane_width)
 
     self.r_poly = r_poly
     self.r_prob = r_prob
