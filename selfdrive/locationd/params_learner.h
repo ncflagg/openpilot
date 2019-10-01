f15862c0d26b08d7a481aeef2b98c608afb2f770
Nate Flagg <ncflagg@gmail.com>
2019-10-01 12:10:21 -0700

    Static sR and tS

--- selfdrive/locationd/params_learner.h
+++ selfdrive/locationd/params_learner.h
@@ -5,12 +5,12 @@
 
 #define MAX_ANGLE_OFFSET (10.0 * DEGREES_TO_RADIANS)
 #define MAX_ANGLE_OFFSET_TH  (9.0 * DEGREES_TO_RADIANS)
-#define MIN_STIFFNESS  0.5
-#define MAX_STIFFNESS  2.0
-#define MIN_SR  0.5
-#define MAX_SR  2.0
-#define MIN_SR_TH  0.55
-#define MAX_SR_TH  1.9
+#define MIN_STIFFNESS  0.9599
+#define MAX_STIFFNESS  0.9601
+#define MIN_SR  1.0
+#define MAX_SR  1.0
+#define MIN_SR_TH  1.0
+#define MAX_SR_TH  1.0
 
 class ParamsLearner {
   double cF0, cR0;
