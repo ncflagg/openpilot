3dd4f88c8dd928355991c94a0dc46f6f4d4360db
Comma Device <device@comma.ai>
2019-10-07 20:49:18 -0700

    Limits tweak (still needs work)

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -22,9 +22,9 @@ ACCEL_SCALE = max(ACCEL_MAX, -ACCEL_MIN)
 # Steer torque limits
 class SteerLimitParams:
   STEER_MAX = 1500
-  STEER_DELTA_UP = 17       # 1.5s time to peak torque
-  STEER_DELTA_DOWN = 17     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
-  STEER_ERROR_MAX = 450     # max delta between torque cmd and torque motor
+  STEER_DELTA_UP = 18       # 1.5s time to peak torque
+  STEER_DELTA_DOWN = 20     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+  STEER_ERROR_MAX = 550     # max delta between torque cmd and torque motor
 
 # Steer angle limits (tested at the Crows Landing track and considered ok)
 ANGLE_MAX_BP = [0., 5.]
